af5e934cc90c5b6fbe931b3017a0d460f87c00a2
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2014-11-07 12:43:44 +0000

    Solucionamos los errores volviendo a una copia de seguridad

--- Question-simple_expec/lib/Question/simple_expec/lista.rb
+++ Question-simple_expec/lib/Question/simple_expec/lista.rb
@@ -1,12 +1,10 @@
-Node = Struct.new(:value, :next, :prev)
+Node = Struct.new(:value, :next)
 class Lista
 
 	attr_reader :head
-	attr_reader :tail
 
 	def initialize(val)	
 		@head = Node.new(val)
-		@tail = Node.new(val)
 	end
 
 	def pop()
@@ -20,23 +18,7 @@ class Lista
 		while (aux.next != nil) do
 			aux = aux.next
 		end
-		aux.prev = aux.next
 		aux.next = Node.new(val,nil)
-		@tail = aux.next
 	end
-
-	def adelante()
-		aux = @head
-		while (aux.next != nil) do
-			aux = aux.next
-		end
-	end
-
-	def atras()
-		aux = @tail
-		while (aux.prev != nil) do
-			aux = aux.prev
-		end
-	end
-	
 end
+

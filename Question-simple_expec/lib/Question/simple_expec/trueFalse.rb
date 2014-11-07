1740a76642e73e67401a1bf9495a8310a1309f18
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2014-11-07 12:50:42 +0000

    Creada clase truefalse

---
+++ Question-simple_expec/lib/Question/simple_expec/trueFalse.rb
@@ -0,0 +1,17 @@
+require_relative "pregunta"
+
+class  TrueFalse
+
+	attr_accessor :truefalses
+
+	def initialize(enun)
+		super(enun)
+		@truefalses = ['a) Cierto','b) Falso']
+	end
+
+	def to_s
+		cadena ="#{@enun}\n"
+		@truefalses.each {|m| cadena += "#{m}\n"}
+		cadena
+        end	
+end
